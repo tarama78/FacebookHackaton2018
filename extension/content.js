def310ebbd9dacd2de11ccaa99ee791fb316d995
tarama78 <tnicolas@student.42.fr>
2018-04-07 21:51:15 +0200

    extension

---
+++ extension/content.js
@@ -0,0 +1,48 @@
+var new_only = 0
+
+function f1() {
+	if (new_only == 1) {
+		console.log('ok');
+		divs = document.getElementsByClassName("clearfix _5va3");
+		for (var i = 0; i < divs.length; i++) {
+			var div = divs[i]
+			if (div == null) {
+				continue
+			}
+			var val = div.textContent;
+	//		console.log(val);
+			if (!val.includes('Le Cercle Scout') && !val.includes('Solidariscout')) {
+				console.log(val);
+				div.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
+			}
+		}
+	}
+}
+
+//function ft_checkbox() {
+//	console.log('serge check box');
+//	var checkBox = document.getElementById("myCheck");
+	// Get the output text
+//	var text = document.getElementById("text");
+//
+	// If the checkbox is checked, display the output text
+//	if (checkBox.checked == true){
+//		new_only = 1
+//	}
+//	else {
+//		new_only = 0
+//	}
+//}
+
+//function sayHiTo(name) {
+//  return function() {
+//    alert("Hi, " + name + "!");
+//  }
+//}
+//
+// Here, invoking the function is correct: it will return a function
+//document.getElementById("checkBox").addEventListener('click', sayHiTo("Bob"));
+
+
+
+window.setInterval(f1, 1000);
