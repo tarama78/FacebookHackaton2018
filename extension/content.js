bb74ac8bb55d626e756e790d2d56e0ff44de1421
Yohan Nacache <nacache.yohan@gmail.com>
2018-04-07 23:39:41 +0200

    ok

--- extension/content.js
+++ extension/content.js
@@ -1,18 +1,46 @@
-var new_only = 0
+var new_only = 1
 
 function f1() {
 	if (new_only == 1) {
-		console.log('ok');
+		//	console.log('ok');
 		divs = document.getElementsByClassName("clearfix _5va3");
+
+
 		for (var i = 0; i < divs.length; i++) {
-			var div = divs[i]
+			var div = divs[i];
 			if (div == null) {
 				continue
 			}
 			var val = div.textContent;
-	//		console.log(val);
-			if (!val.includes('Le Cercle Scout') && !val.includes('Solidariscout')) {
-				console.log(val);
+			var span_test = div.getElementsByClassName("fwb fcg").item(0);
+			var alink = span_test.children.item(0);
+			//console.log(alink);
+			var href = $(alink).attr('href');
+			//console.log(href);
+			var getJSON = function(url, callback) {
+				var xhr = new XMLHttpRequest();
+				xhr.open('GET', url, true);
+				xhr.responseType = 'json';
+				xhr.onload = function() {
+					var status = xhr.status;
+					if (status === 200) {
+						callback(null, xhr.response);
+					} else {
+						callback(status, xhr.response);
+					}
+				};
+				xhr.send();
+			};
+			getJSON(href,
+				function(err, data) {
+					if (err !== null) {
+						alert('Something went wrong: ' + err);
+					} else {
+						alert('Your query count: ' + data.query.count);
+					}
+				});
+			if (!val.includes('NowThis') && !val.includes('Seeker')) {
+				//console.log(val);
 				div.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = 'none';
 			}
 		}
